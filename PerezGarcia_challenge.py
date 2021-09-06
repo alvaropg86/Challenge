27d790be48212300b66d205071f050dee49fffa6
alvaro <a.perezgarcia@itecriocuarto.org.ar>
2021-09-06 02:22:33 -0300

    first commit

---
+++ PerezGarcia_challenge.py
@@ -0,0 +1,133 @@
+import re
+
+unidades = ["", "uno", "dos" ,"tres" ,"cuatro" ,"cinco" ,
+            "seis" ,"siete" ,"ocho" ,"nueve"]
+number_list = ["0", "1", "2" ,"3" ,"4" ,"5" ,
+            "6" ,"7" ,"8" ,"9"] 
+especiales = ["once", "doce","trece","catorce", "quince",
+              "dieciseis", "diecisiete", "dieciocho", "diecinueve", "veintiuno"
+                , "veintidos","veintitres","veinticuatro", "veinticinco",
+              "veintiseis", "venitisiete", "veintiocho", "veintinueve"]
+special_list = ["11", "12", "13" ,"14" ,"15" ,"16" ,
+            "17" ,"18" ,"19", "21", "22", "23" ,"24" ,"25" ,"26" ,
+            "27" ,"28" ,"29"]        
+decenas = ["", "diez","veinte","treinta","cuarenta","cincuenta", "sesenta",
+           "setenta", "ochenta", "noventa"]
+decenas_list = ["10","20","30","40","50", "60",
+           "70", "80", "90"]
+centenas = ["","ciento", "doscientos" ,"trescientos" ,"cuatrocientos" ,"quinientos" ,
+            "seiscientos" ,"setecientos" ,"ochocientos" ,"novecientos"]
+miles = ["","mil", "dos mil" ,"tres mil" ,"cuatro mil" ,"cinco mil" ,
+            "seis mil" ,"siete mil" ,"ocho mil" ,"nueve mil"]         
+
+def unaCifra(number):
+    if number == "0":
+        return "cero"
+    if len(number) == 1:
+        for i in range(len(unidades)): #obtiene numeros del 0 al 9
+            if number == number_list[i]:
+                return unidades[i]
+
+def dosCifras(number):
+    if int(number[0]) == 0:
+        return unidades[int(number[1])]
+    if int(number[1]) == 0:
+        return decenas[int(number[0])]
+    if int(number) > 10 and int(number) <= 29: #obtiene los especiales (11 a 29)
+        for i in range(len(especiales)):
+            if number == special_list[i]:
+                return especiales[i]            
+    if int(number) > 29 and int(number) < 100:
+        x = decenas[int(number[0])]
+        u = unidades[int(number[1])]
+        return (x + " y " + u)
+                                            
+def tresCifras(number):
+    if number == "100":
+        return "cien"
+    else:    
+        cent = number[0]
+        for i in range(len(unidades)): #obtiene numeros del 0 al 9
+            if cent == number_list[i]:
+                c = centenas[i]     
+        return (c + " " + dosCifras(number[1:3]))
+
+def cuatroCifras(number):
+    if number == "1000":
+        return "mil"
+    else:    
+        cent = number[0]
+        for i in range(len(unidades)): #obtiene numeros del 0 al 9
+            if cent == number_list[i]:
+                c = miles[i]     
+        return (c + " " + tresCifras(number[1:4]))
+
+def cincoCifras(number):
+    if number == "10000":
+        return " diez mil"
+    else:     
+        return dosCifras(number[0:2]) + " mil " + tresCifras(number[2:5])
+
+def seisCifras(number):
+    if number[0:3] != "000":
+        return tresCifras(number[0:3]) + " mil " + tresCifras(number[3:6])
+    if number[0:3] == "000":
+        return tresCifras(number[0:3]) + tresCifras(number[3:6])    
+
+def sieteCifras(number):
+    if number == "1000000":
+        return "un millon"
+    else:
+        if int(number[0]) == 1:
+            return "un millon " + seisCifras(number[1:7])
+        if int(number[0]) > 1:
+            return (str(unaCifra(number[0])) + " millones ") + seisCifras(number[1:7])
+
+def ochoCifras(number):
+        return str(dosCifras(number[0:2]) + " millones " + seisCifras(number[2:8]))
+
+def nueveCifras(number):
+        return str(tresCifras(number[0:3]) + " millones " + seisCifras(number[3:9]))
+
+def diezCifras(number):
+    return "mil millones"
+
+number= input("ingrese el numero: ")
+
+if number[0:2] == "00" or number[0:1] =="0" and number != "0":
+    print("no deben contabilizarse ceros al comienzo del numero para que la aplicación funcione")    
+elif int(number) <0 or int(number) >1000000000:
+    print("el numero se encuentra fuera de rango de la aplicación (0 a 1.000.000.000)")  
+else:
+    if len(number) == 1:
+        resultado= unaCifra(number)
+    if len(number) == 2:    
+        resultado= dosCifras(number)
+    if len(number) == 3:    
+        resultado= tresCifras(number)
+    if len(number) == 4:    
+        resultado= cuatroCifras(number)
+    if len(number) == 5:    
+        resultado= cincoCifras(number)
+    if len(number) == 6:    
+        resultado= seisCifras(number)
+    if len(number) == 7:    
+        resultado= sieteCifras(number) 
+    if len(number) == 8:
+        resultado= ochoCifras(number)
+    if len(number) == 9:
+        resultado= nueveCifras(number)
+    if len(number) == 10:
+        resultado= diezCifras(number)
+    resultadoFinal = re.sub(' +', ' ', resultado)
+    if "uno m" in resultadoFinal:
+        resultadoFinal2=resultadoFinal.replace("uno m","un m")
+        print (resultadoFinal2)
+    else:
+        print(resultadoFinal)
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Challenge
+# Challenge - README
+La siguiente aplicación ha sido creada en Python el 03/09/2021 por Alvaro Perez García para cumplimentar con un "Challenge" propuesto en la materia "Practica Profesionalizante I" de la carrera de Desarrollador de Software del ITEC Río Cuarto (segundo año).
+La aplicación permite convertir números en cifras a números en letras.
+
+consideraciones a tener en cuenta:
+- La aplicación se ejecuta por linea de comandos.
+- Solo funciona si cargamos numeros.
+- Solo trabaja con números enteros.
+- Funciona para el siguiente rango de numeros: de 0 a 1.000.000.000.
+- No funciona si se aplican ceros antes del número a convertir.
+
+Instrucciones para su uso:
+1) Se debe correr la aplicación.
+2) Automaticamente la aplicación solicitara que se tipee el numero en cifras a ser convertido a letras.
+3) tipeamos el numero en cifras que queremos convertir a letras
+4) Aplicamos ENTER
+5) el sistema automaticamente hará la conversión y mostrará resultado. 
+
+FIN
+
+
